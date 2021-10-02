8b615265e48ee9e23f49624e61161c4e160874b8
nhutlam123 <nguyenhoangnhutlam58@gmail.com>
2021-10-02 20:51:39 +0700

    update ần 3â
    sdsda

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	string str = "Xin chào" ; // biến chữ
+	int i = 1;  //biến số chẵn
+	long l = 0.1;  // số lẻ
+	double d = 1.2;  //cặp chản
+	cout << "Hello word\n";
+	system("pause");
+	return 0;
+}
